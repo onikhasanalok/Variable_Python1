f940c9313a4fe304de3b0a557587cd60a1da6e73
Onik Hasan <onikhasanalok123@gmail.com>
2022-07-29 02:48:23 +0600

    Change

--- Variable_01.py
+++ Variable_01.py
@@ -8,3 +8,35 @@ e=' Test Repositories!'
 d=a+b+c+e
 
 print(d)
+
+#Varibale Using for Python 
+#How to print strings and integer type Variable
+
+Name="Onik Hasan"
+Age=26
+Country="Bangladesh"
+
+print(Name)
+print(Age)
+print(Country)
+
+
+#Varibale Using for Python 
+#All types of variable and type
+
+Name="Onik Hasan"
+Age=26
+Country="Bangladesh"
+BACC=18315111
+BBL=55555.55
+Alive=True
+Job=False
+
+
+print(Name,type(Name))
+print(Age,type(Age))
+print(Country,type(Country))
+print(BACC,type(BACC))
+print(BBL,type(BBL))
+print(Alive,type(Alive))
+print(Job,type(Job))
